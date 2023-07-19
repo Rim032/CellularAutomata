199fa56fd67ec3c60d6c71c82a5dfde57ddaf28c
Rim032 <45215785+Rim032@users.noreply.github.com>
2023-07-19 10:20:52 -0400

    Update celluar_automata.cpp

--- Src/celluar_automata.cpp
+++ Src/celluar_automata.cpp
@@ -6,8 +6,6 @@
 #define WIDTH 128
 #define HEIGHT 128
 
-int cell_grid[WIDTH][HEIGHT];
-
 using namespace std;
 
 class celluar_automata : public olc::PixelGameEngine
@@ -19,7 +17,9 @@ public:
 	}
 
 private:
-	void handle_nearby_cells(int cell_grid[][128], int &pos_x, int &pos_y)
+	int cell_grid[WIDTH][HEIGHT];
+
+	void handle_nearby_cells(int cell_grid[][128], const int &pos_x, const int &pos_y)
 	{
 		int neighbor_sum = cell_grid[pos_x - 1][pos_y - 1] +
 			cell_grid[pos_x - 0][pos_y - 1] + cell_grid[pos_x + 1][pos_y - 1] +
@@ -38,6 +38,62 @@ private:
 		else if (neighbor_sum == 3 && cell_grid[pos_x][pos_y] == 0)
 		{
 			cell_grid[pos_x][pos_y] = 1;
+		}
+	}
+
+	void draw_game()
+	{
+		for (int x = 0; x < WIDTH; x++)
+		{
+			for (int y = 0; y < HEIGHT; y++)
+			{
+				handle_nearby_cells(cell_grid, x, y);
+
+				if (cell_grid[x][y] == 1)
+				{
+					Draw(x, y, olc::WHITE);
+				}
+				else
+				{
+					Draw(x, y, olc::BLACK);
+				}
+			}
+		}
+	}
+
+	void reset_grid()
+	{
+		FillRect(0, 0, ScreenWidth(), ScreenHeight(), olc::BLANK);
+		for (int x = 0; x < WIDTH; x++)
+		{
+			for (int y = 0; y < HEIGHT; y++)
+			{
+				cell_grid[x][y] = 0;
+			}
+		}
+
+		OnUserCreate();
+	}
+
+	void user_controls()
+	{
+		if (GetKey(olc::Key::R).bHeld)
+		{
+			reset_grid();
+		}
+
+		if (GetKey(olc::Key::SHIFT).bHeld)
+		{
+			this_thread::sleep_for(5ms);
+		}
+		else if (!GetKey(olc::Key::SHIFT).bHeld)
+		{
+			this_thread::sleep_for(40ms);
+		}
+
+		if (GetKey(olc::Key::CTRL).bHeld)
+		{
+			this_thread::sleep_for(80ms);
 		}
 	}
 
@@ -60,52 +116,8 @@ public:
 			return true;
 		}
 
-		if (GetKey(olc::Key::R).bHeld)
-		{
-			FillRect(0, 0, ScreenWidth(), ScreenHeight(), olc::BLANK);
-			for (int x = 0; x < WIDTH; x++)
-			{
-				for (int y = 0; y < HEIGHT; y++)
-				{
-					cell_grid[x][y] = 0;
-				}
-			}
-
-			OnUserCreate();
-			return true;
-		}
-
-		if (GetKey(olc::Key::SHIFT).bHeld)
-		{
-			this_thread::sleep_for(5ms);
-		}
-		else if (!GetKey(olc::Key::SHIFT).bHeld)
-		{
-			this_thread::sleep_for(40ms);
-		}
-
-		if (GetKey(olc::Key::CTRL).bHeld)
-		{
-			this_thread::sleep_for(80ms);
-		}
-
-
-		for (int x = 0; x < WIDTH; x++)
-		{
-			for (int y = 0; y < HEIGHT; y++)
-			{
-				handle_nearby_cells(cell_grid, x, y);
-				
-				if (cell_grid[x][y] == 1)
-				{
-					Draw(x, y, olc::WHITE);
-				}
-				else
-				{
-					Draw(x, y, olc::BLACK);
-				}
-			}
-		}
+		draw_game();
+		user_controls();
 
 		return true;
 	}
